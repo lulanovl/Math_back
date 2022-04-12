9b829ed6bfe341f6f9a7cb13d2f3648a2ff159f0
lulanovl <lulanovvvl@gmail.com>
2022-04-13 01:23:39 +0600

    added all

---
+++ backend/APP/mathematics.py
@@ -0,0 +1,36 @@
+from sympy import *
+from numpy import random
+x = Symbol('x')
+import warnings
+warnings.filterwarnings("error")
+warnings.filterwarnings("ignore", category=UserWarning)
+
+def newtonMethod(input_function, first_guess, number_of_iterations):
+    try:
+        input_function = sympify(input_function.replace('e', 'E')) #replaces 
+        f = lambdify(x, input_function) #lambdify expression of the input function
+        f_d = lambdify(x, diff(input_function, x))  #lambdify expression of the derivative of the input function
+        x_i = first_guess
+        for i in range(number_of_iterations):
+            x_i = x_i - (f(x_i)/f_d(x_i))
+        if f(x_i) > 0.000001:
+            return "Please check the function. Probably, it does not have any roots"
+        else:
+            return x_i
+    except RuntimeWarning:
+        return "Please change your first guess. Perhaps, the method came across with vertex or new x_i are diverging instead of converging."
+
+
+
+
+
+
+# We have to create a "symbol" called x, as you will have any (a,h or y variables) you should write 'var = Symbol ('var')' and ect
+def diffMethod(f: str):
+    x = Symbol('x') #these are variables
+    f = f.replace('e', 'E')
+    f = sympify(f) #this is input (be careful with writing the power of exp, because here we dont use usal (**) but just take in breakets 
+    f_prime = f.diff(x)  
+    f = lambdify(x, f) #idetifiying respect to which variable we are taking variable
+    #print in the space (f_prime) it will give the answer
+    return str(f_prime)
---
+++ backend/APP/views.py
@@ -0,0 +1,46 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import NewtonSerializer, DiffSerializer
+from .mathematics import newtonMethod, diffMethod
+# Create your views here.
+
+@api_view(['POST',])
+def newton_list(request):
+    if request.method == 'POST':
+        deserialized = NewtonSerializer(data=request.data)
+        if deserialized.is_valid():
+            equation = deserialized.data['equation']
+            first = int(deserialized.data['first'])
+            second = int(deserialized.data['second'])
+            answer = newtonMethod(equation, first, second)
+            return Response(answer, status=status.HTTP_201_CREATED)
+        else:
+            return Response(deserialized.error_messages)
+
+
+@api_view(['POST',])
+def diff_list(request):
+    if request.method == 'POST':
+        deserialized = DiffSerializer(data=request.data)
+        if deserialized.is_valid():
+            equation = deserialized.data['equation']
+           
+            answer = diffMethod(equation)
+            return Response(answer, status=status.HTTP_201_CREATED)
+        else:
+            return Response(deserialized.error_messages)
+    
+
+    # if request.method == 'GET':
+    #     answer = {
+    #         "equation":"x",
+    #         "first":200,
+    #         "second":300
+    #     }
+
+    #     serializer = NewtonSerializer(data=answer)
+    #     serializer.is_valid()
+    #     return Response(serializer.validated_data, status=status.HTTP_204_NO_CONTENT)
+    
+
